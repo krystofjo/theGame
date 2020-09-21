6e4e0309488cc16035c21f9acb61508240782c71
krystofjo <43005473+krystofjo@users.noreply.github.com>
2020-09-21 20:23:03 +0200

    Level9

--- Assets/Scripts/Objectives/SegmentController.cs
+++ Assets/Scripts/Objectives/SegmentController.cs
@@ -8,6 +8,7 @@ public class SegmentController : MonoBehaviour
     public bool isSwitchedOn;
     public enum Direction {X10, Y10, Z10};
     public Direction direction;
+    public float delayTime = 0; 
 
     Animator anim;
  
@@ -20,23 +21,31 @@ public class SegmentController : MonoBehaviour
         }
     }
 
-
-
-
-
     public void UpdateStatus()
     {  
         if(isSwitchedOn)
         {   
-            Debug.Log("Segement Switched On");
-            anim.SetBool(direction.ToString(), true);
+            Invoke("SwitchingOn", delayTime);
+
         }
         if(!isSwitchedOn)
         {
+            Invoke("SwitchingOff", delayTime);
+        }
+
+    }
+
+    void SwitchingOn()
+    {
+            Debug.Log("Segement Switched On");
+            anim.SetBool(direction.ToString(), true);
+    }
+
+    void SwitchingOff()
+    {
             Debug.Log("Segement Switched Off");
             anim.SetBool("X10", false);
             anim.SetBool("Y10", false);
             anim.SetBool("Z10", false);
-        }
     }
 }
